6e621a2b02ad034fe6ea3d7e213fe9f02db8d26f
christopherhurt <chris526@vt.edu>
2019-03-25 15:09:58 -0400

    Game singleton class implementation

--- hurtEngine/core/game.cpp
+++ hurtEngine/core/game.cpp
@@ -13,31 +13,75 @@ Game * Game::instance() {
 }
 
 void Game::initGame(int width, int height, char * title, Vec3 * clearColor) {
-	// TODO
+	initContext();
+	GLFWwindow* window = createWindow(width, height, title);
+	checkGLAD();
+	initConfig(clearColor);
+
+	gameLoop(window);
+	cleanUp();
 }
 
 void Game::close(int status) {
-	// TODO
+	if (status) {
+		cerr << "Game terminated early" << endl;
+		exit(-1);
+	}
+	else {
+		closed = true;
+	}
 }
 
 // Private
 
 void Game::initContext() {
-	// TODO
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+}
+
+GLFWwindow * Game::createWindow(int width, int height, char * title) {
+	GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
+
+	if (window == NULL) {
+		cerr << "Failed to create GLFW window" << endl;
+		glfwTerminate();
+		exit(-1);
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height) { glViewport(0, 0, width, height); });
+
+	return window;
 }
 
 void Game::checkGLAD() {
-	// TODO
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		cerr << "Failed to initialize GLAD" << endl;
+		exit(-1);
+	}
 }
 
 void Game::initConfig(Vec3 * clearColor) {
-	// TODO
+	glClearColor(clearColor->x, clearColor->y, clearColor->z, 1.0f);
 }
 
 void Game::gameLoop(GLFWwindow * window) {
-	// TODO
+	while (!(glfwWindowShouldClose(window) || closed)) {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		// TODO: update input here
+
+		// TODO: Add additional updating here
+
+		glfwSwapBuffers(window);
+	}
 }
 
 void Game::cleanUp() {
-	// TODO
+	// TODO: Add additional resource clean up here
+
+	glfwTerminate();
 }
--- hurtEngine/core/game.h
+++ hurtEngine/core/game.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <iostream>
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 #include "hurtEngine/math/vec3.h"
+
+using namespace std;
 
 class Game {
 public:
@@ -10,7 +13,11 @@ public:
 	void initGame(int width, int height, char * title, Vec3 * clearColor);
 	void close(int status);
 private:
+	bool closed = false;
+
+	inline Game() {};
 	void initContext();
+	GLFWwindow * createWindow(int width, int height, char * title);
 	void checkGLAD();
 	void initConfig(Vec3 * clearColor);
 	void gameLoop(GLFWwindow * window);
