dcf75187a57f2bf22bf893d8d8713d065dc47b0c
christopherhurt <chris526@vt.edu>
2019-04-16 13:39:37 -0400

    Most of Entity fragment shader

--- hurtEngine/shaders/entityFragment.glsl
+++ hurtEngine/shaders/entityFragment.glsl
@@ -12,9 +12,9 @@ out vec4 outColor;
 struct Material {
 	vec3 color;
 	sampler2D diffuseMap;
-	bool useDiffuseMap;
+	int useDiffuseMap;
 	sampler2D lightMap;
-	bool useLightMap;
+	int useLightMap;
 	float ambient;
 	float diffuse;
 	float specular;
@@ -51,5 +51,53 @@ uniform PointLight pointLights[MAX_POINT_LIGHTS];
 uniform int numPointLights;
 
 void main() {
-	// TODO
+	vec3 baseColor;
+	if(material.useDiffuseMap) {
+		vec4 texColor = texture(material.diffuseMap, texCoords);
+		if(texColor.a == 0.0) {
+			discard;
+		} else {
+			baseColor = texColor.rgb;
+		}
+	} else {
+		baseColor = material.color;
+	}
+
+	float ambientFactor;
+	float diffuseFactor;
+	float specularFactor;
+	if(material.useLightMap) {
+		vec4 lightValues = texture(material.lightMap, texCoords);
+		ambientFactor = lightValues.r;
+		diffuseFactor = lightValues.g;
+		specularFactor = lightValues.b;
+	} else {
+		ambientFactor = material.ambient;
+		diffuseFactor = material.diffuse;
+		specularFactor = material.specular;
+	}
+	int shininessFactor = material.shininess;
+
+	vec3 ambientColor = vec3(0.0);
+	vec3 diffuseColor = vec3(0.0);
+	vec3 specularColor = vec3(0.0);
+	for(int i = 0; i < numDirectionalLights; i++) {
+		DirectionalLight light = directionalLights[i];
+		vec3 lightShade = light.basic.color * light.basic.intensity;
+
+		ambientColor += ambientFactor * lightShade;
+
+		float diffuseDot = max(dot(normal, -light.direction), 0.0);
+		diffuseColor += diffuseFactor * diffuseDot * lightShade;
+
+		vec3 reflection = reflect(light.direction, normal);
+		float specularEffect = pow(max(dot(normalize(toCam), reflection), 0.0), shininessFactor);
+		specularColor += specularFactor * specularEffect * lightShade;
+	}
+	for(int i = 0; i < numPointLights; i++) {
+		// TODO: point light calculations
+	}
+
+	vec3 finalColor = baseColor * (ambientColor + diffuseColor) + specularColor;
+	outColor = vec4(finalColor, 1.0);
 }
